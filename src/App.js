ab9ea5f91111901e1af4fe51389f610917aaca2c
souhailelk <elkaissi.souhail.me@gmail.com>
2020-08-28 22:43:44 +0000

    Add Article Card

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import Article from './components/Article'
+import ArticleComponent from './components/Article'
+import ArticleCard from './components/ArticleCard'
+import Article from './models/Article'
 
 function App() {
-  let content = {}
-  const e = <p>From Elecrow. I have two of their first CrowPi device and I use them in demonstrations and talks all the time, especially when talking to students.
+  const content = <p>From Elecrow. I have two of their first CrowPi device and I use them in demonstrations and talks all the time, especially when talking to students.
   They have a Kickstarter ending this week for the new CrowPi2. It's an update of the original CrowPi which was basically a Raspberry Pi in a tiny suitcase...instead the CrowPi2 looks like a laptop! The magical part is that the full exploration and sensor board is hidden underneath a wireless keyboard with trackpad, held on with magnets!
   The CrowPi folks sent me an early CrowPi2 because I was a vocal supporter of the previous version, so I did a LIVE video unboxing on twitter as well as a follow up software video on the CrowPi2. I am not being paid for this review and I always am honest with y'all about the stuff I like. I plan on buying a few for my kids' public school science class.</p>
-  content.title = "Reviewing the Elecrow CrowPi2 Raspberry Pi Laptop and STEM Education Platform"
-  content.content = <div>{e}{e}{e}{e}{e}{e}</div>
-  content.infos = "published on 20/08/2020"
+  const title = "Reviewing the Elecrow CrowPi2 Raspberry Pi Laptop and STEM Education Platform"
+  const date = "20/08/2020"
+  const tags = ["photography","other","other","other","other"]
+  const article = new Article(title, content, date, tags)
   return (
     <div>
-      <Article  content={content} />
+      <ArticleCard article={article} />
+      <ArticleComponent  article={article} />
     </div>
   );
 }
---
+++ src/models/Article.js
@@ -0,0 +1,9 @@
+class Article{
+    constructor(title, content, date, tags){
+        this.title = title;
+        this.content = content;
+        this.date = date;
+        this.tags = tags;
+    }
+}
+export default Article;
