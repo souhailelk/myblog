fd211741b0cbb6ad9e57aab201ebdc6493c82a65
souhailelk <elkaissi.souhail.me@gmail.com>
2020-11-08 19:40:15 +0100

    Expand NavigationBar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -16,15 +16,15 @@ function NavigationBar(props) {
     return (
         <nav class="flex items-center justify-between flex-wrap bg-black p-3">
             <div class="flex items-center flex-shrink-0 text-white mr-6">
-                <span class="font-semibold text-xl tracking-tight">S\E</span>
+                <span class="font-semibold text-2xl tracking-tight">S\E</span>
             </div>
             <div class="block lg:hidden">
-                <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"  onClick={() => setNavbarOpen(!navbarOpen)}>
+                <button class="flex items-center px-3 py-2 border rounded text-white border-white hover:text-white hover:border-white"  onClick={() => setNavbarOpen(!navbarOpen)}>
                     <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
                 </button>
             </div>
             <div class={"w-full block flex-grow lg:flex lg:items-center lg:w-auto"+ (navbarOpen ? " flex" : " hidden")} id="Menu" >
-                <div class="text-sm lg:flex-grow">
+                <div class="text-xl lg:flex-grow">
                    {items}
                 </div>
             </div>
