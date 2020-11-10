d174a6c160bddb167b451f87fb4aee7875e66832
souhailelk <elkaissi.souhail.me@gmail.com>
2020-11-11 00:01:42 +0100

    Make icon bar a link

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -16,16 +16,16 @@ function NavigationBar(props) {
     return (
         <nav class="flex items-center justify-between flex-wrap bg-black p-3">
             <div class="flex items-center flex-shrink-0 text-white mr-6">
-                <span class="font-semibold text-2xl tracking-tight">S\E</span>
+                <span class="font-semibold text-2xl tracking-tight"><a href="/">S\E</a></span>
             </div>
             <div class="block lg:hidden">
-                <button class="flex items-center px-3 py-2 border rounded text-white border-white hover:text-white hover:border-white"  onClick={() => setNavbarOpen(!navbarOpen)}>
+                <button class="flex items-center px-3 py-2 border rounded text-white border-white hover:text-white hover:border-white" onClick={() => setNavbarOpen(!navbarOpen)}>
                     <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
                 </button>
             </div>
-            <div class={"w-full block flex-grow lg:flex lg:items-center lg:w-auto"+ (navbarOpen ? " flex" : " hidden")} id="Menu" >
+            <div class={"w-full block flex-grow lg:flex lg:items-center lg:w-auto" + (navbarOpen ? " flex" : " hidden")} id="Menu" >
                 <div class="text-xl lg:flex-grow">
-                   {items}
+                    {items}
                 </div>
             </div>
         </nav>
