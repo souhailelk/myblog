0f9a860a5fec1144aaaeb113ae39bf4587a4a62c
souhailelk <elkaissi.souhail.me@gmail.com>
2021-05-13 20:05:32 +0000

    [NavigationBar] remove uselss css

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -7,24 +7,23 @@ function NavigationBar(props) {
     let items = []
     navigationBarItems.forEach(navigationBarItem => {
         items.push(
-            <a href={navigationBarItem.route} class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray mr-4">
+            <a href={navigationBarItem.route} class="block lg:inline-block mt-4 mr-4 lg:mt-0 text-xl">
                 {navigationBarItem.name}
             </a>
         )
     });
 
     return (
-        <nav class="flex items-center justify-between flex-wrap bg-black p-3">
-            <div class="flex items-center flex-shrink-0 text-white mr-6">
-                <span class="font-semibold text-2xl tracking-tight"><a href="/">S\E</a> beta </span>
-            </div>
-            <div class="block lg:hidden">
-                <button class="flex items-center px-3 py-2 border rounded text-white border-white hover:text-white hover:border-white" onClick={() => setNavbarOpen(!navbarOpen)}>
-                    <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
-                </button>
-            </div>
-            <div class={"w-full block flex-grow lg:flex lg:items-center lg:w-auto" + (navbarOpen ? " flex" : " hidden")} id="Menu" >
-                <div class="text-xl lg:flex-grow">
+        <nav class="flex  justify-between flex-wrap text-white bg-black p-3">
+            <span class="items-center mr-6 text-2xl tracking-tight font-semibold"><a href="/">S\E</a> beta</span>
+            <button class="lg:hidden px-3 py-2 border rounded border-white" onClick={() => setNavbarOpen(!navbarOpen)}>
+                <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <title>Menu</title>
+                    <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                </svg>
+            </button>
+            <div class={"w-full block flex-grow lg:flex lg:items-center lg:w-auto" + (navbarOpen ? " flex" : " hidden")}>
+                <div>
                     {items}
                 </div>
             </div>
