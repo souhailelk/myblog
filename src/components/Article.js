edef899e9be6b1594fe63c6f3a9704b0a6d329b1
souhailelk <elkaissi.souhail.me@gmail.com>
2021-06-13 21:33:05 +0100

    [Article] Make content overflow

--- src/components/Article.js
+++ src/components/Article.js
@@ -8,10 +8,10 @@ function Article(props) {
     return (
 
         <div className="flex justify-center overflow-hidden rounded">
-            <div className="box-content max-w-screen-lg shadow-lg px-6 py-4">
+            <div className="box-content max-w-screen-lg shadow-lg px-6 py-4 overflow-auto">
                 <img alt="Article" class="object-cover h-1/2 w-full" src="https://picsum.photos/600/400/?random" />
                 <div class="font-bold justify-center text-2xl mm:text-4xl break-words">{article.title}</div>
-                <div class="flex text-sm md:text-lg tracking-widest break-words ml-3 mb-3">published on {article.date}</div>
+                <div class="text-sm md:text-lg tracking-widest break-words ml-3 mb-3">published on {article.date}</div>
                 <div class="text-gray-700 text-xs md:text-base leading-relaxed text-justify font-mono break-words px-2 py-6">{article.content}</div>
                 <div class="px-6 py-4">{tags}</div>
             </div>
