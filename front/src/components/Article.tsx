b27b87b652c6ddbf733b99c3a9c018e2d3b6744c
souhailelk <elkaissi.souhail.me@gmail.com>
2024-06-14 01:53:02 +0100

    Fix it

--- front/src/components/Article.tsx
+++ front/src/components/Article.tsx
@@ -7,47 +7,48 @@ import ArticlesRepository from '../repositories/ArticlesRepository';
 
 
 function Article() {
-    type ArticleParam = {id:string};
-    const {id} = useParams<ArticleParam>();
-    const [article, setArticle] = useState<ArticleModel>();
-    const [loading, setLoading] = useState(true);
-    useEffect(() => {
-      const fetchArticles = async () => {
-        try {
-          let repository = new ArticlesRepository();
-          const response = await repository.getArticleById(id);
-          setArticle(response);
-        } catch (error) {
-          console.error(error);
-        } finally {
-          setLoading(false);
-        }
-      };
-  
-      fetchArticles();
-    }, [id]);
+  type ArticleParam = { id: string };
+  const { id } = useParams<ArticleParam>();
+  const [article, setArticle] = useState<ArticleModel>();
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        let repository = new ArticlesRepository();
+        const response = await repository.getArticleById(id);
+        setArticle(response);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-    if (loading) {
-      return <div>Loading...</div>;
-    }
-    if (!article) {
-        return <div>Article not found</div>;
-    }
-    const tags:React.JSX.Element[] = []
+    fetchArticles();
+  }, [id]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (!article) {
+    return <div>Article not found</div>;
+  }
+  const tags: React.JSX.Element[] = []
+  if (article.tags)
     article.tags.forEach(tag =>
-        tags.push(<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs md:text-sm font-semibold text-gray-700 mr-2 mt-3">#{tag}</span>));
-    return (
+      tags.push(<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs md:text-sm font-semibold text-gray-700 mr-2 mt-3">#{tag}</span>));
+  return (
 
-        <div className="flex justify-center overflow-hidden rounded">
-            <div className="box-content max-w-screen-lg shadow-lg px-6 py-4 overflow-auto">
-                <img alt="Article" className="object-cover h-1/2 w-full" src="https://picsum.photos/600/400/?random" />
-                <div className="font-bold justify-center text-2xl mm:text-4xl break-words">{article.title}</div>
-                <div className="text-sm md:text-lg tracking-widest break-words ml-3 mb-3">published on {format(article.date,'MMMM do, yyyy H:mm:ss a')}</div>
-                <div className="text-gray-700 text-xs md:text-base leading-relaxed text-justify font-mono break-words px-2 py-6">{article.content}</div>
-                <div className="px-6 py-4">{tags}</div>
-            </div>
-        </div>
-    )
+    <div className="flex justify-center overflow-hidden rounded">
+      <div className="box-content max-w-screen-lg shadow-lg px-6 py-4 overflow-auto">
+        <img alt="Article" className="object-cover h-1/2 w-full" src="https://picsum.photos/600/400/?random" />
+        <div className="font-bold justify-center text-2xl mm:text-4xl break-words">{article.title}</div>
+        <div className="text-sm md:text-lg tracking-widest break-words ml-3 mb-3">published on {format(article.date, 'MMMM do, yyyy H:mm:ss a')}</div>
+        <div className="text-gray-700 text-xs md:text-base leading-relaxed text-justify font-mono break-words px-2 py-6">{article.content}</div>
+        <div className="px-6 py-4">{tags}</div>
+      </div>
+    </div>
+  )
 }
 
 export default Article;
